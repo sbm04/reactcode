3775501ade5ea35e9ef870018c0bcdbfa63ee67f
Shubham Bharti <shubhambharti04@gmail.com>
2025-04-12 14:35:39 +0530

    first commit 1

---
+++ React Project/javascript/javascripttut.js
@@ -0,0 +1,15 @@
+var a=9;
+var b=10;
+
+console.log(" Summary of JavaScript" , a+b);
+
+const obj = {
+
+    name:"JavaScript",
+    version:"ES6",      
+    type:"Scripting Language",
+}
+
+for(let key in obj){
+    console.log(key + ":" + obj.key);
+}
